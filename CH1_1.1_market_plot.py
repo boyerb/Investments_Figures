98b4422c1c96e6afc3e283f49f95b551ffec28f9
Andrew Hall <andrewmartinhall2@gmail.com>
2024-12-23 07:11:07 -0800

    CH1 1.1 Market Plot

---
+++ CH1_1.1_market_plot.py
@@ -0,0 +1,26 @@
+import yfinance as yf
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = yf.download(
+    tickers=["SPY"], 
+    start="2005-01-01", 
+    end="2023-12-31"
+)
+
+data = data.stack().reset_index()
+
+print(data.columns)
+
+data['Return'] = data['Close'].pct_change()
+
+data['Gross Return'] = data['Return'] + 1
+
+data['Cummulative Return'] = data['Gross Return'].cumprod()
+
+data['Investment Value'] = 1000 * data['Cummulative Return']
+
+sns.lineplot(data, x='Date', y='Investment Value')
+
+plt.savefig('plots/CH1-1.1_market_plot.png')
+plt.show()
